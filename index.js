8f2eba3af2a67f1ea9d635dc0e6e8b7961dce449
leemhoon00 <leemhoon00@naver.com>
2023-10-07 21:58:12 +0900

    feat: add build label

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const githubLabelSync = require("github-label-sync");
 require("dotenv").config();
 
 const labelName = "simple"; // labels 폴더에 있는 레이블 파일 중 사용할 레이블 명
-const repoName = "leemhoon00/github-customLabel"; // 레이블을 적용할 레포지토리 ex) user/repo
+const repoName = "leemhoon00/typevocab-server"; // 레이블을 적용할 레포지토리 ex) user/repo
 
 const labelData = yaml.load(
   fs.readFileSync(String("./labels/" + labelName + ".yaml"), "utf8")
